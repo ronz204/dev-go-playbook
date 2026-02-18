ce887ce59444d9be5e9d1e6deb1327de7e126c2c
roney ruiz <roneyruizrojas@gmail.com>
2026-02-17 21:12:44 -0600

    feat: learn about conditions

--- cmd/main.go
+++ cmd/main.go
@@ -8,4 +8,28 @@ func main() {
 
 	fmt.Println(text)
 	fmt.Println(inferred)
+
+	if maybe := true; maybe {
+		fmt.Println("Maybe is true")
+	} else {
+		fmt.Println("Maybe is false")
+	}
+
+	isWeekend, isHoliday, isRaining := true, false, true
+
+	if isWeekend && !isHoliday && !isRaining {
+		fmt.Println("It's a good day for a picnic!")
+	}
+
+	if isWeekend && (isHoliday || isRaining) {
+		fmt.Println("It's a complicated day!")
+	}
+
+	if isWeekend || isHoliday {
+		fmt.Println("It's a day off!")
+	}
+
+	if !isRaining {
+		fmt.Println("It's not raining, you can go outside!")
+	}
 }
